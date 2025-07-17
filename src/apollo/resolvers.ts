41d1ca3141fdc3e7a3f97780890a52fee112df28
Matt Gunnin <mgunnin@esports.one>
2025-07-17 12:19:27 -0500

    fix: RESUME_DATA read only error

--- src/apollo/resolvers.ts
+++ src/apollo/resolvers.ts
@@ -1,12 +1,12 @@
-import { Query, Resolver } from "type-graphql";
-import { RESUME_DATA } from "../data/resume-data";
-import { resumeDataToGraphQL } from "../lib/types";
-import { Me } from "./type-defs";
+import { Query, Resolver } from "type-graphql"
+import { RESUME_DATA } from "../data/resume-data"
+import { resumeDataToGraphQL } from "../lib/types"
+import { Me } from "./type-defs"
 
 @Resolver(() => Me)
 export class MeResolver {
   @Query(() => Me)
   me(): Me {
-    return resumeDataToGraphQL(RESUME_DATA);
+    return resumeDataToGraphQL(RESUME_DATA as any)
   }
 }
